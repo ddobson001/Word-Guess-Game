c3317d6875087e0819045296ac524e76f909dbfe
david dobson <david.m.dobson@hotmail.com>
2018-12-02 22:00:45 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,24 @@
 //Global Variables
 // ==================================================================================
 // create an array of words
-let word = [`fat`, `lazy`, `hungry`];
+let word = [
+    `bulbasaur`,
+    `Charmander`,
+    `squirtle`,
+    `butterfree`,
+    `pidgeotto`,
+    `clefairy`,
+    `vulpix`,
+    `jigglypuff`,
+    `alakazam`,
+    `geodude`,
+    `haunter`,
+    `koffing`,
+    `eevee`,
+    `pikachu`,
+    `mew`,
+    `magikarp`,
+];
 //Set limit on tries
 let maxTries = 4;
 // Choose word randomly
@@ -24,7 +41,7 @@ console.log(rightWord);
 let generateUnderscore = () => {
     for (let i = 0; i < choosenWord.length; i++) {
         underScore.push(`_`)
-        ;    
+            ;
     }
     return underScore;
 }
@@ -33,24 +50,24 @@ let generateUnderscore = () => {
 document.addEventListener(`keypress`, (event) => {
     let keyword = String.fromCharCode(event.keyCode);
     //if User guess is right
-    if(choosenWord.indexOf(keyword) > -1) {
-// replace underscore with right letter
-underScore[choosenWord.indexOf(keyword)] = keyword;
-docUnderScore[0].innerHTML = underScore.join(` `);
-docRightGuess[0].innerHTML = rightWord;
-// check to see if user word matches guesses
-if(underScore.join(``) == choosenWord){
-    alert(`You Win`);
-}
-}
-else { 
-  wrongWord.push(keyword);
-  docUnderScore[0].innerHTML = underScore.join(` `);
-  docWrongGuess[0].innerHTML = wrongWord;
-}   
+    if (choosenWord.indexOf(keyword) > -1) {
+        // replace underscore with right letter
+        underScore[choosenWord.indexOf(keyword)] = keyword;
+        docUnderScore[0].innerHTML = underScore.join(` `);
+        docRightGuess[0].innerHTML = rightWord;
+        // check to see if user word matches guesses
+        if (underScore.join(``) == choosenWord) {
+            alert(`You Win`);
+        }
+    }
+    else {
+        wrongWord.push(keyword);
+        docUnderScore[0].innerHTML = underScore.join(` `);
+        docWrongGuess[0].innerHTML = wrongWord;
+    }
 });
 
-docUnderScore[0].innerHTML= generateUnderscore().join(``);
+docUnderScore[0].innerHTML = generateUnderscore().join(``);
 // Check if guess is right
 // if right right push to right array
 // if wrong push to wrong array
