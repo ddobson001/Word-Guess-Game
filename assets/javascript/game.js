f7fafb51f59c14e75aa0792e14c72dcea23127f5
david dobson <david.m.dobson@hotmail.com>
2018-12-06 22:46:53 -0500

    update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -20,54 +20,56 @@ let word = [
     `magikarp`,
 ];
 //Set limit on tries
-let maxTries = 4;
+let guesses = 4;
+
 // Choose word randomly
 let randomWord = Math.floor(Math.random() * word.length);
 let choosenWord = word[randomWord];
-let rightWord = [];
 let wrongWord = [];
-let underScore = [];
 
 // DOM Manipulation
 let docUnderScore = document.getElementsByClassName(`underScore`);
 let docRightGuess = document.getElementsByClassName(`rightGuess`);
 let docWrongGuess = document.getElementsByClassName(`wrongGuess`);
 
+docUnderScore.innerHTML = generateUnderscore().join(``);
+var newUnderscoreArray = generateUnderscore();
+console.log(newUnderscoreArray);
+
 //Main
 // ==================================================================================
 
-console.log(rightWord);
 // Create underscores based on legnth of word
-let generateUnderscore = () => {
+function generateUnderscore() {
+    let underScore = [];
     for (let i = 0; i < choosenWord.length; i++) {
-        underScore.push(`_`)
-            ;
+        underScore.push(`_`);
     }
     return underScore;
 }
+
 
 // Get users guess
 document.addEventListener(`keypress`, (event) => {
     let keyword = String.fromCharCode(event.keyCode);
     //if User guess is right
-    if (choosenWord.indexOf(keyword) > -1) {
+    if  (choosenWord.indexOf(keyword) > -1) {
         // replace underscore with right letter
-        underScore[choosenWord.indexOf(keyword)] = keyword;
-        docUnderScore[0].innerHTML = underScore.join(` `);
-        docRightGuess[0].innerHTML = rightWord;
-        // check to see if user word matches guesses
-        if (underScore.join(``) == choosenWord) {
-            alert(`You Win`);
-        }
+        for (var i = 0; i < choosenWord.length; i++ ) {
+            if (choosenWord[i] == keyword) {
+                underScore[i] = keyword;
+                docUnderScore.innerHTML = underScore.join(` `);
+            } 
+        } 
     }
     else {
         wrongWord.push(keyword);
-        docUnderScore[0].innerHTML = underScore.join(` `);
-        docWrongGuess[0].innerHTML = wrongWord;
+        docWrongGuess.innerHTML = wrongWord;
+        guesses--;
+        document.getElementById("guesses-left").innerHTML = guesses;
     }
 });
 
-docUnderScore[0].innerHTML = generateUnderscore().join(``);
 // Check if guess is right
 // if right right push to right array
 // if wrong push to wrong array
